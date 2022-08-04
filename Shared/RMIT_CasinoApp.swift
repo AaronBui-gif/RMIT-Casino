e658f41d743119f5c161d25c47d208a6ccc84fa3
AaronBui <aaronhuy2509@gmail.com>
2022-08-04 13:28:03 +0700

    Initial Commit

---
+++ Shared/RMIT_CasinoApp.swift
@@ -0,0 +1,17 @@
+//
+//  RMIT_CasinoApp.swift
+//  Shared
+//
+//  Created by Huy, Bui Thanh on 04/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct RMIT_CasinoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
